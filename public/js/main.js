72f24dd2682ec8e5af040513348c6691c55cf867
Maximiliano De Santis <maximilianodesantis@gmail.com>
2022-06-01 21:30:38 -0300

    limpieza de codigo para la seccion de login y creado de usuario

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,122 @@
 
-import {controlUserValidation,createNewUser} from "./user.js"
-import {Validation} from "./validation.js"
+'use strict'
+
+// IMPORTS
+import { createUser, userLogIn } from "./user.js";
+//import { validateData } from "./validation.js";
+
+const $userSignIn = document.getElementById("sign-in-user"),
+$passwordSignIn = document.getElementById("sign-in-password"),
+$btnSignIn = document.getElementById("sign-in-btn");
 
 
-document.addEventListener('click', e => {
-    //e.preventDefault();
-    
-    if (e.target.id === "login-btn") {
-        e.preventDefault()
-        const $inputUserName = document.getElementById('login-user').value;
-        const $inputUserPassword = document.getElementById('login-password').value;
 
-        if (controlUserValidation($inputUserName,$inputUserPassword)) {
+document.addEventListener("click" , e => {
+    // BOTON INICIAR SESION
+
+    if (e.target.matches('#login-btn')) {
+        e.preventDefault();
+
+        // FALTA AGREGAR CONTROL DE DATOS INGRESADOS
+
+       if (userLogIn(
+            document.getElementById("login-user").value,
+            document.getElementById("login-password").value,
+        )) {
             window.location.href = "../pages/portfolio.html"
         } else {
-            const $notificationForm = document.getElementById("signUp-notification");
-            $notificationForm.classList.add("bg-red","show-block");
-            $notificationForm.innerText = 'Datos incorrectos';
+                const $notificationForm = document.getElementById("signUp-notification");
+                $notificationForm.classList.add("bg-red","show-block");
+                $notificationForm.innerText = 'Datos incorrectos';
         }
+
+
+
     }
 
-    if (e.target.id === "create-newUser") {
-        e.preventDefault()
+    if (e.target.matches('#create-newUser')) {
+        // BOTON CREAR USUARIO
 
-        const $createUserName = document.getElementById("create-user").value
-        const $createUserEmail = document.getElementById("create-email").value
-        const $createUserPhone = document.getElementById("create-phone").value
-        const $createUserPassword = document.getElementById("create-password").value
+        e.preventDefault();
 
-        let control = createNewUser($createUserName,$createUserEmail,$createUserPhone,$createUserPassword);
-        
-        console.log(control)
-        if (control) {
+        // FALTA AGREGAR CONTROL DE DATOS INGRESADOS
+
+        if (createUser(
+            document.getElementById("create-user").value,
+            document.getElementById("create-password").value,
+            document.getElementById("create-email").value,
+        )) {
             const $notificationForm = document.getElementById("signUp-notification");
             $notificationForm.classList.add("bg-green","show-block");
             $notificationForm.innerText = 'Usuario creado con exito';
-        }
-        if (!control) {
+        } else {
             const $notificationForm = document.getElementById("signUp-notification");
             $notificationForm.classList.add("bg-red","show-block");
             $notificationForm.innerText = 'Error';
-            // Corregir para detallar mejor lista de errores
         }
-    }
 
+
+
+    }
 
 
 
 })
+
+
+
+
+
+
+
+
+// import {controlUserValidation,createNewUser} from "./user.js"
+// import {Validation} from "./validation.js"
+
+
+// document.addEventListener('click', e => {
+//     //e.preventDefault();
+    
+//     if (e.target.id === "login-btn") {
+//         e.preventDefault()
+//         const $inputUserName = document.getElementById('login-user').value;
+//         const $inputUserPassword = document.getElementById('login-password').value;
+
+//         if (controlUserValidation($inputUserName,$inputUserPassword)) {
+//             window.location.href = "../pages/portfolio.html"
+//         } else {
+//             const $notificationForm = document.getElementById("signUp-notification");
+//             $notificationForm.classList.add("bg-red","show-block");
+//             $notificationForm.innerText = 'Datos incorrectos';
+//         }
+//     }
+
+//     if (e.target.id === "create-newUser") {
+//         e.preventDefault()
+
+//         const $createUserName = document.getElementById("create-user").value
+//         const $createUserEmail = document.getElementById("create-email").value
+//         const $createUserPhone = document.getElementById("create-phone").value
+//         const $createUserPassword = document.getElementById("create-password").value
+
+//         let control = createNewUser($createUserName,$createUserEmail,$createUserPhone,$createUserPassword);
+        
+//         console.log(control)
+//         if (control) {
+//             const $notificationForm = document.getElementById("signUp-notification");
+//             $notificationForm.classList.add("bg-green","show-block");
+//             $notificationForm.innerText = 'Usuario creado con exito';
+//         }
+//         if (!control) {
+//             const $notificationForm = document.getElementById("signUp-notification");
+//             $notificationForm.classList.add("bg-red","show-block");
+//             $notificationForm.innerText = 'Error';
+//             // Corregir para detallar mejor lista de errores
+//         }
+//     }
+
+
+
+
+// })
 
--- public/js/user.js
+++ public/js/user.js
@@ -1,125 +1,243 @@
+
 'use strict'
 
-import { Validation } from "./validation.js";
+
+class newUser {
+    constructor(user,pass,email){
+        this.user = user;
+        this.pass = pass;
+        this.email = email;
+    }
+}
+
+const usersDB = [];
+const userActive = {
+};
 
 
-// const users = {
-//     user01:{
-//         Usuario: "Admin",
-//         Mail: "admin@admin.com",
-//         Phone: "1234567890",
-//         Password: "qwerty"
-//     },
+
+// TRAER USUARIOS ALMACENADOS EN JSON
+export const getUsersDB = () => {
+    let usersFromJSON = JSON.parse(localStorage.getItem("usersDB"));
+
+    for (const obj in usersFromJSON) {
+            let create = new newUser(usersFromJSON[obj].user, usersFromJSON[obj].pass, usersFromJSON[obj].email);
+            usersDB.push(create);
+        }
+}
+
+
+// GENERAR UNA BASE DE DATOS EN JSON
+export const saveUsersDB = (data) => {
+
+    let userDBToJSON = JSON.stringify(data);
+    localStorage.setItem("usersDB", userDBToJSON)
+}
+
+// CREAR NUEVO USUARIO Y GUARDARLO EN JSON
+export const createUser = (user,password,email) => {
+    if (!checkUser(user,email)) {
+        usersDB.push(new newUser(user,password,email)) ;
+        saveUsersDB(usersDB);
+        return true
+    }
+    return false
+}
+
+
+// FUNCION INICIO DE SESION
+export const userLogIn = (inputUser,inputPassword) => {
+   // getUsersDB(); // ACTUALIZAR LISTA DE USUARIOS
+    for (const user in usersDB) {
+        if ((usersDB[user].user === inputUser) || (usersDB[user].email === inputUser)) {
         
+            if (usersDB[user].pass === inputPassword) {
+                userActive.name = usersDB[user].user;
+                userActive.email = usersDB[user].email;
+                userActive.pass = usersDB[user].pass;
+                return true
+            }
+        }
+        
+    }
+    return false
+
+}
+
+
+
+
+// CONTROLAR SI UN USUARIO EXISTE
+export const checkUser = (user,email) => {
+
+    for (const obj in usersDB) {
+        if (usersDB[obj].user === user.toLowerCase() || usersDB[obj].email === email.toLowerCase()) {
+            // SI ENCUENTRA EL USUARIO O MAIL EJECUTA MENSAJE DE NOTIFICACION
+            
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+
+
+
+
+getUsersDB();
+console.log(usersDB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict'
+
+// import { Validation } from "./validation.js";
+
+
+// // const users = {
+// //     user01:{
+// //         Usuario: "Admin",
+// //         Mail: "admin@admin.com",
+// //         Phone: "1234567890",
+// //         Password: "qwerty"
+// //     },
+        
+// // }
+
+// const usuarios = []
+
+// // console.log(Object.keys(users).length)
+
+// const actualizarListaUsuarios = () => {
+//     let usuariosGuardados = localStorage.getItem("UsuariosCreados");
+//     usuariosGuardados = JSON.parse(usuariosGuardados)
+
+//     for (const obj in usuariosGuardados) {
+//         usuarios[obj] = {
+//             Usuario: usuariosGuardados[obj].Usuario,
+//             Mail: usuariosGuardados[obj].Mail,
+//             Phone: usuariosGuardados[obj].Phone,
+//             Password: usuariosGuardados[obj].Password,
+//         }
+//     }
+
+
 // }
 
-const usuarios = []
-
-// console.log(Object.keys(users).length)
-
-const actualizarListaUsuarios = () => {
-    let usuariosGuardados = localStorage.getItem("UsuariosCreados");
-    usuariosGuardados = JSON.parse(usuariosGuardados)
-
-    for (const obj in usuariosGuardados) {
-        usuarios[obj] = {
-            Usuario: usuariosGuardados[obj].Usuario,
-            Mail: usuariosGuardados[obj].Mail,
-            Phone: usuariosGuardados[obj].Phone,
-            Password: usuariosGuardados[obj].Password,
-        }
-    }
-
-
-}
 
 
 
-
-class usersRegister {
-    constructor(user,email,phone,password){
-        this.usuario = user,
-        this.email = email,
-        this.phone = phone,
-        this.password = password;
-        //FALTA AGREGAR DNI
-    }
-}
-
+// class usersRegister {
+//     constructor(user,email,phone,password){
+//         this.usuario = user,
+//         this.email = email,
+//         this.phone = phone,
+//         this.password = password;
+//         //FALTA AGREGAR DNI
+//     }
+// }
 
 
-export const controlUserValidation = (usuarioIngresado= undefined, contraseñaIngresada= undefined)=> {
-    actualizarListaUsuarios() // actualizar valores de usuarios creados
-    if (!Validation.empty({usuarioIngresado,contraseñaIngresada})) {
-        return false
-    }
+
+// export const controlUserValidation = (usuarioIngresado= undefined, contraseñaIngresada= undefined)=> {
+//     actualizarListaUsuarios() // actualizar valores de usuarios creados
+//     if (!Validation.empty({usuarioIngresado,contraseñaIngresada})) {
+//         return false
+//     }
     
 
-    for (const user in usuarios){
+//     for (const user in usuarios){
 
-        if ((usuarios[user].Usuario.toLowerCase() === usuarioIngresado.toLowerCase()) || (usuarios[user].Mail.toLowerCase() === usuarioIngresado.toLowerCase())) {
+//         if ((usuarios[user].Usuario.toLowerCase() === usuarioIngresado.toLowerCase()) || (usuarios[user].Mail.toLowerCase() === usuarioIngresado.toLowerCase())) {
 
-            if (usuarios[user].Password === contraseñaIngresada) {
-                console.log("USUARIO CREADO!")
-               return true;
-            }
-            else {
-                return false;
-            }
+//             if (usuarios[user].Password === contraseñaIngresada) {
+//                 console.log("USUARIO CREADO!")
+//                return true;
+//             }
+//             else {
+//                 return false;
+//             }
 
-        }
-        else return false;
-    }
+//         }
+//         else return false;
+//     }
 
-}
+// }
 
-export const createNewUser = (user = undefined,email = undefined,phone = undefined,password = undefined) => {
-    if (!Validation.empty({user,email,phone,password})) {
-        return false;
-    }
-        // ERROR VALIDACION MAIL A CHEQUEAR
+// export const createNewUser = (user = undefined,email = undefined,phone = undefined,password = undefined) => {
+//     if (!Validation.empty({user,email,phone,password})) {
+//         return false;
+//     }
+//         // ERROR VALIDACION MAIL A CHEQUEAR
 
-    // if (!Validation.email(email)) {
-    //     console.log("email" , email);
-    //     return false;
-    // }
-
-
-
-
-    // Actualizar lista de usuarios ya creados para agregar uno nuevo
-    actualizarListaUsuarios()
+//     // if (!Validation.email(email)) {
+//     //     console.log("email" , email);
+//     //     return false;
+//     // }
 
 
 
-    // CONTROLAR QUE EL USUARIO NO ESTE YA CREADO FALTA MEJORAR CODIGO
-    for (const obj in usuarios) {
-        if (usuarios[obj].Usuario === user.toLowerCase() || usuarios[obj].Mail === email.toLowerCase()) {
-            return false
-        }
-    }
+
+//     // Actualizar lista de usuarios ya creados para agregar uno nuevo
+//     actualizarListaUsuarios()
 
 
-    // GENERAR UN USARIO NUEVO EN EL OBJETO
-    usuarios.push(
-        {
-            Usuario: user.toLowerCase(),
-            Mail: email.toLowerCase(),
-            Phone: phone,
-            Password: password
-        }
-    )
+
+//     // CONTROLAR QUE EL USUARIO NO ESTE YA CREADO FALTA MEJORAR CODIGO
+//     for (const obj in usuarios) {
+//         if (usuarios[obj].Usuario === user.toLowerCase() || usuarios[obj].Mail === email.toLowerCase()) {
+//             return false
+//         }
+//     }
+
+
+//     // GENERAR UN USARIO NUEVO EN EL OBJETO
+//     usuarios.push(
+//         {
+//             Usuario: user.toLowerCase(),
+//             Mail: email.toLowerCase(),
+//             Phone: phone,
+//             Password: password
+//         }
+//     )
     
 
-    // GUARDAR USUARIO NUEVO EN LOCAL STORAGE
-    let usersJson = JSON.stringify(usuarios);
-    localStorage.setItem("UsuariosCreados", usersJson)
+//     // GUARDAR USUARIO NUEVO EN LOCAL STORAGE
+//     let usersJson = JSON.stringify(usuarios);
+//     localStorage.setItem("UsuariosCreados", usersJson)
 
 
-    actualizarListaUsuarios()
-    return true
+//     actualizarListaUsuarios()
+//     return true
 
-}
+// }
 
 
 
